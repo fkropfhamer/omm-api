5539055a6f8bc77e69c3de0d2c230ac88a826756
fkropfhamer <fabian.kropfhamer@sblum.de>
2020-11-28 13:28:47 +0100

    added get meme by id

--- src/routes/api/v1/meme.ts
+++ src/routes/api/v1/meme.ts
@@ -6,51 +6,75 @@ import Meme from '../../../models/meme';
 const router = express.Router();
 
 router.post('/', async (req, res) => {
-    const {url = '', bottom = '', top = '', name = uuidv4()} = req.body;
+    try {
+        const {url = '', bottom = '', top = '', name = uuidv4()} = req.body;
 
-    const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
-    const image = await Jimp.read(url);
-    image.print(font, 10, 10, top);
-    image.print(font, 100, 100, bottom);
+        const font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
+        const image = await Jimp.read(url);
+        image.print(font, 10, 10, top);
+        image.print(font, 100, 100, bottom);
 
-    const fileName = uuidv4() + '.' + image.getExtension();
-    const fileUrl = 'http://localhost:8000/uploads/memes/' + fileName;
+        const fileName = uuidv4() + '.' + image.getExtension();
+        const fileUrl = 'http://localhost:8000/uploads/memes/' + fileName;
 
-    await image.writeAsync('./uploads/memes/' + fileName);
+        await image.writeAsync('./uploads/memes/' + fileName);
 
-    const meme = new Meme({
-        url: fileUrl,
-        name,
-    })
-
-    await meme.save();
-
-    res.json({
-        status: true,
-        message: 'meme created',
-        data: {
+        const meme = new Meme({
             url: fileUrl,
             name,
-        }
-    });
+        })
+
+        await meme.save();
+
+        res.json({
+            status: true,
+            message: 'meme created',
+            data: {
+                id: meme.id,
+                url: fileUrl,
+                name,
+            }
+        });
+    } catch(err) {
+        res.status(500).send(err);
+        console.log(err);
+}
 });
 
 router.get('/', (req, res) => {
-    Meme.find({}, (err, memes) => {
-        if (err) {
-            res.send({
-                status: false,
-                message: err
-            });
-        } else {
-           res.json({
-                status: true,
-                data: {
-                    memes
-                }
-            });
-        }
-    });
+    if (req.query.id) {
+        Meme.findById(req.query.id, (err, meme) => {
+            if (err) {
+                res.send({
+                    status: false,
+                    message: err
+                });
+            } else {
+               res.json({
+                    status: true,
+                    data: {
+                        meme
+                    }
+                });
+            }
+        })
+    } else {
+        Meme.find({}, (err, memes) => {
+            if (err) {
+                res.send({
+                    status: false,
+                    message: err
+                });
+            } else {
+               res.json({
+                    status: true,
+                    data: {
+                        memes
+                    }
+                });
+            }
+        });
+    }
 });
 
 export default router;
