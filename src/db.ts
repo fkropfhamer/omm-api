266e449b54740633d71c7c6edf111f29b3b6d38e
hanna.k <hanna.k0616@gmail.com>
2021-01-23 13:37:11 +0100

    alternative database on cloud

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export function connectDb() {
-    return mongoose.connect('mongodb://localhost/memes', {useNewUrlParser: true, useUnifiedTopology: true});
-};
-
+  const DB_local = "mongodb://localhost/memes";
+  const DB_cloud =
+    "mongodb+srv://ommproject20202021:omm20202021@cluster0.zugan.mongodb.net/ommproject20202021?retryWrites=true&w=majority";
+  return mongoose
+    .connect(DB_cloud, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((con) => {
+      console.log("Db connected");
+    });
+}
