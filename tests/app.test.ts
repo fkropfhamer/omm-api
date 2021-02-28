0848d32746b980b74b9cbb92361705230c733ccd
fkropfhamer <fabian.kropfhamer@sblum.de>
2021-02-28 17:15:09 +0100

    added tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -31,7 +31,7 @@ describe('app', () => {
             })
     })
 
-    test('get meme meme found', (done) => {
+    test('get meme found', (done) => {
         const mockMeme = {
             id: "1234",
             likes: 3
@@ -49,6 +49,116 @@ describe('app', () => {
                     data: {
                         meme: mockMeme
                     },
+                });
+                done();
+            })
+    })
+
+    test('like meme, meme not found', (done) => {
+        mockingoose(Meme).toReturn(null, 'findOne');
+
+        request(app).post("/api/v1/meme/like")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: false,
+                    message: "meme not found :(",
+                });
+                done();
+            })
+    })
+
+    test('like meme, meme found', (done) => {
+        const mockMeme = {
+            likes: 0
+        }
+
+        mockingoose(Meme).toReturn(mockMeme, 'findOne');
+
+        request(app).post("/api/v1/meme/like")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: true,
+                    message: "meme liked",
+                });
+                done();
+            })
+    })
+
+    test('dislike meme, meme not found', (done) => {
+        mockingoose(Meme).toReturn(null, 'findOne');
+
+        request(app).post("/api/v1/meme/dislike")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: false,
+                    message: "meme not found :(",
+                });
+                done();
+            })
+    })
+
+    test('dislike meme, meme found', (done) => {
+        const mockMeme = {
+            likes: 0,
+            dislikes: 3
+        }
+
+        mockingoose(Meme).toReturn(mockMeme, 'findOne');
+
+        request(app).post("/api/v1/meme/dislike")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: true,
+                    message: "meme disliked",
+                });
+                done();
+            })
+    })
+
+    test('comment meme, meme not found', (done) => {
+        mockingoose(Meme).toReturn(null, 'findOne');
+
+        request(app).post("/api/v1/meme/comment")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: false,
+                    message: "meme not found :(",
+                });
+                done();
+            })
+    })
+
+    test('like meme, meme found', (done) => {
+        const mockMeme = {
+            likes: 0,
+            comments: []
+        }
+
+        mockingoose(Meme).toReturn(mockMeme, 'findOne');
+
+        request(app).post("/api/v1/meme/comment")
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: true,
+                    message: "comment saved",
                 });
                 done();
             })
