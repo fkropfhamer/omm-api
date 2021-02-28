783fa4172757594e65ff21d9084ca8b0b00cd356
fkropfhamer <fabian.kropfhamer@gmail.com>
2021-02-28 16:37:25 +0100

    added some tests

--- tests/app.test.ts
+++ tests/app.test.ts
@@ -1,8 +1,10 @@
 import app from '../src/app';
 import request from 'supertest'
+import mockingoose from 'mockingoose';
+import Meme from '../src/models/meme';
 
 describe('app', () => {
-    it('returns json', (done) => {
+    test('status page works', (done) => {
         request(app).get("/api/v1/")
             .expect('Content-Type', /json/)
             .expect(200)
@@ -12,4 +14,43 @@ describe('app', () => {
                 done();
             });
     });
+
+    test('get meme meme not found', (done) => {
+        mockingoose(Meme).toReturn(null, 'findOne');
+
+        request(app).get("/api/v1/meme/search/test")
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: false,
+                    message: "meme not found",
+                });
+                done();
+            })
+    })
+
+    test('get meme meme found', (done) => {
+        const mockMeme = {
+            id: "1234",
+            likes: 3
+        }
+
+        mockingoose(Meme).toReturn(mockMeme, 'findOne');
+
+        request(app).get("/api/v1/meme/search/test")
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, res) => {
+                if (err) done(err);
+                expect(res.body).toMatchObject({
+                    status: true,
+                    data: {
+                        meme: mockMeme
+                    },
+                });
+                done();
+            })
+    })
 });
